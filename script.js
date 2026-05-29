97343c851f65d7e71663526c4701807e07a1bcb2
Nithish-Raj-2000 <nithish2000m@gmail.com>
2026-05-29 17:39:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// MOBILE MENU
+
+const menuBtn = document.getElementById('menuBtn');
+const navLinks = document.getElementById('navLinks');
+
+menuBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+
+// COUNTER ANIMATION
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+
+  counter.innerText = '0';
+
+  const updateCounter = () => {
+
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText;
+
+    const increment = target / 100;
+
+    if(count < target){
+
+      counter.innerText = `${Math.ceil(count + increment)}`;
+
+      setTimeout(updateCounter, 20);
+
+    } else {
+
+      counter.innerText = target;
+
+    }
+
+  };
+
+  updateCounter();
+
+});
